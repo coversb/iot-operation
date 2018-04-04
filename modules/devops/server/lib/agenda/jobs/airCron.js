ff6b8906e445c9f982352c0c0bc3afc176b1f0b6
markhuyong <markhuyong@gmail.com>
2018-04-04 16:56:49 +0800

    fix: job log description

--- modules/devops/server/lib/agenda/jobs/airCron.js
+++ modules/devops/server/lib/agenda/jobs/airCron.js
@@ -4,7 +4,7 @@ module.exports.init = function init(agenda) {
   agenda.define('airCon', function (job, done) {
     const jobRunner = new JobRunner(job);
      jobRunner.run();
-    console.log("job-1 data:", job.attrs.data);
+    console.log("airCon data:", job.attrs.data);
     setTimeout(function () {
       jobRunner.destroy();
       done()
