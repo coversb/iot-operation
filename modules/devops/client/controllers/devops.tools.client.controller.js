bfc1d77494b775ffbceafebcfdfc710d6c6daec2
ChenHao <chenhao@parkbox.cn>
2018-05-16 11:39:50 +0800

    Added confirm window to avoid misoperation

--- modules/devops/client/controllers/devops.tools.client.controller.js
+++ modules/devops/client/controllers/devops.tools.client.controller.js
@@ -5,9 +5,9 @@
     .module('devops.tools')
     .controller('DevopsToolsController', DevopsToolsController);
 
-  DevopsToolsController.$inject = ['$scope', '$state', 'Authentication', 'DevopsProt', 'Notification'];
+  DevopsToolsController.$inject = ['$window', '$http', '$scope', '$state', 'Authentication', 'DevopsSettings', 'DevopsProt', 'Notification'];
 
-  function DevopsToolsController($scope, $state, Authentication, DevopsProt, Notification) {
+  function DevopsToolsController($window, $http, $scope, $state, Authentication, DevopsSettings, DevopsProt, Notification) {
 
     var vm = this;
 
@@ -21,10 +21,12 @@
       ['open', 1]
     ]);
 
+    vm.availableBoxMap = new Map();
     vm.sendDevAirConCommand = sendDevAirConCommand;
     vm.sendDevRTOCommand = sendDevRTOCommand;
 
     init();
+    updateAvailableUID();
 
     function init() {
       // If user is not signed in then redirect back home
@@ -42,13 +44,53 @@
 
     function showSendRes(data, status) {
       if (status === 200) {
-        Notification.success({ message: '<i class="glyphicon glyphicon-ok"></i> 发送成功!' });
+        Notification.success({message: '<i class="glyphicon glyphicon-ok"></i> 发送成功!'});
       } else {
-        Notification.error({ message: data, title: '<i class="glyphicon glyphicon-remove"></i> 发送失败!' });
+        Notification.error({message: data, title: '<i class="glyphicon glyphicon-remove"></i> 发送失败!'});
       }
     }
 
+    function updateAvailableUID() {
+      var config = {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+      var data = {
+        'pageNum': 1,
+        'pageSize': 1024
+      };
+      $http.post(DevopsSettings.backboneURL + DevopsSettings.boxListAPI, data, config)
+        .success(function (data) {
+          if (Array.isArray(data.data)) {
+            vm.availableBoxMap.clear();
+            for (var idx = 0; idx < data.data.length; ++idx) {
+              vm.availableBoxMap[data.data[idx].base.uniqueId] = data.data[idx].base.name;
+            }
+          }
+        })
+        .error(function (e, status) {
+          console.log('Error:' + e + ',' + status);
+        });
+    }
+
+    function sendCommandConfirm(text) {
+      var boxName = vm.availableBoxMap[vm.devUID];
+      if (boxName === undefined) {
+        boxName = '未知场馆';
+      }
+      boxName += '(' + vm.devUID + ')';
+      return $window.confirm('确定向"' + boxName + '"发送？\n' + text);
+    }
+
     function sendDevAirConCommand() {
+      var confText = '供电：' + $('#devAirConPwrMode option:selected').text() + '\n'
+        + '模式：' + $('#devAirConWorkMode option:selected').text() + '\n'
+        + '风力：' + $('#devAirConWindMode option:selected').text() + '\n'
+        + '温度：' + $('#devAirConTemperature option:selected').text() + '\n';
+      if (sendCommandConfirm(confText) === false) {
+        return;
+      }
       var param = {
         uid: vm.devUID,
         pwrMode: vm.devAirConPwrMode,
@@ -62,6 +104,20 @@
     }
 
     function sendDevRTOCommand(act, ctrl) {
+      var confText = '';
+      if (act === 'door') {
+        confText = '开盒门';
+      } else if (act === 'deviceBox') {
+        if (ctrl === 'open') {
+          confText = '开设备箱';
+        } else if (ctrl === 'close') {
+          confText = '关设备箱';
+        }
+      }
+
+      if (sendCommandConfirm(confText) === false) {
+        return;
+      }
       var param = {
         uid: vm.devUID,
         cmd: actMap.get(act).toString(10),
